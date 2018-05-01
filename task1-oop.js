e4f30e5d49f1c8b4a9de9ff05add8bc7c86d0df6
Zoryana Makeeva <zoryana.dev@gmail.com>
2018-05-01 15:43:17 +0300

    refactor rename

--- task1-oop.js
+++ task1-oop.js
@@ -2,27 +2,27 @@
     button.addEventListener("click", function () {
         const input = textArea.value.trim();
         const output = new TextParser(input);
-        quantity.textContent = output.getQuantity();
+        quantity.textContent = output.getWordsQuantity();
         max.textContent = output.getMaxLength();
         min.textContent = output.getMinLength();
         average.textContent = output.getAverageLength()
     });
 
     function TextParser(input) {
-        const inputToArray = input.split(/[^a-zа-я]+/gi),
-              arrayOfLengths = inputToArray.map(value => value.toString().length);
-        this.getQuantity = function () {
-             return inputToArray.length;
+        const words = input.split(/[^a-zа-я]+/),
+              wordsLengths = words.map(value => value.toString().length);
+        this.getWordsQuantity = function () {
+             return words.length;
         };
         this.getMaxLength = function () {
-             return Math.max(...arrayOfLengths);
+             return Math.max(...wordsLengths);
         };
         this.getMinLength = function () {
-             return Math.min(...arrayOfLengths);
+             return Math.min(...wordsLengths);
         };
         this.getAverageLength = function () {
-             const reducedArray = arrayOfLengths.reduce((prev, current) => prev + current);
-             return Math.round(reducedArray/inputToArray.length);
+             const reducedArray = wordsLengths.reduce((prev, current) => prev + current);
+             return Math.round(reducedArray/words.length);
         };
     }
 }());
