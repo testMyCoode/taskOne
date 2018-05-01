cb9d6f2d2899e407e5bdfe6673904ced19b740e8
Zoryana Makeeva <zoryana.dev@gmail.com>
2018-05-01 15:15:51 +0300

    refactor rename

--- task1-oop.js
+++ task1-oop.js
@@ -1,14 +1,14 @@
 (function () {
     button.addEventListener("click", function () {
         const input = textArea.value.trim();
-        const output = new Calculation(input);
+        const output = new TextParser(input);
         quantity.textContent = output.getQuantity();
         max.textContent = output.getMaxLength();
         min.textContent = output.getMinLength();
         average.textContent = output.getAverageLength()
     });
 
-    function Calculation(input) {
+    function TextParser(input) {
         const inputToArray = input.split(/[^a-zа-я]+/gi),
               arrayOfLengths = inputToArray.map(value => value.toString().length);
         this.getQuantity = function () {
